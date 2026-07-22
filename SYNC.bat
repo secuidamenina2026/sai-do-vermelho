43284a9e8b1d496edc55ab7efc6b61b99f4a5bd5
Eduardo Jorge <loucospelobem2019@gmail.com>
2026-07-22 19:25:54 -0300

    Sync

--- SYNC.bat
+++ SYNC.bat
@@ -1,13 +1,11 @@
 @echo off
 cd /d "%~dp0"
-echo sync_log.txt>> .gitignore
-echo SYNC.bat>> .gitignore
-echo *.tar.gz>> .gitignore
-echo ==== SYNC3 %date% %time% ==== >> sync_log.txt
+echo ==== SYNC4 %date% %time% ==== >> sync_log.txt
+rmdir /s /q ".git\rebase-merge" >nul 2>&1
+rmdir /s /q ".git\rebase-apply" >nul 2>&1
 del /s /q ".git\*.lock" >nul 2>&1
-git rebase --abort >nul 2>&1
 git add -A >nul 2>&1
-git -c user.email=loucospelobem2019@gmail.com -c user.name="Eduardo Jorge" commit -m "Diagnostico gratuito + esteira de conversao" >> sync_log.txt 2>&1
+git -c user.email=loucospelobem2019@gmail.com -c user.name="Eduardo Jorge" commit -m "Sync" >> sync_log.txt 2>&1
 git -c user.email=loucospelobem2019@gmail.com -c user.name="Eduardo Jorge" pull --rebase --autostash origin master >> sync_log.txt 2>&1
 git push origin master >> sync_log.txt 2>&1
-echo ==== FIM SYNC3 ==== >> sync_log.txt
+echo ==== FIM SYNC4 ==== >> sync_log.txt
